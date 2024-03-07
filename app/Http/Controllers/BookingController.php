6e58ffa0b166d3bd4f72f5eaecebe3992623ee9c
Scott Drinkwater <s.p.drinkwater@gmail.com>
2024-03-07 10:19:29 +0000

    Controller for CRUD of bookings

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateBookingRequest;
+use App\Http\Resources\BookingResource;
+use App\Models\Booking;
+use App\Models\Car;
+use App\Repositories\BookingRepository;
+use App\Services\AgePolicyService;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class BookingController extends Controller
+{
+    private BookingRepository $bookingRepository;
+    private AgePolicyService $agePolicyService;
+    
+    public function __construct(
+        BookingRepository $bookingRepository,
+        AgePolicyService $agePolicyService
+    ) {
+        $this->bookingRepository = $bookingRepository;
+        $this->agePolicyService = $agePolicyService;
+    }
+
+    public function store(CreateBookingRequest $request)
+    {
+        $age = (int) $request->input('age');
+        $carId = $request->input('car_id');
+
+        $car = Car::find($carId);
+
+        $additionalCost = $this->agePolicyService->getAdditionCost($age);
+
+        $booking = Booking::make($request->validated());
+        $booking->cost = $car->cost + $additionalCost;
+
+        $overlappingBookingCount = $this->bookingRepository->countOverlapping(
+            $booking->from_date, 
+            $booking->to_date,
+            $car
+        );
+
+        if ($overlappingBookingCount > 0) {
+            return response()->json(['error' => 'A booking already exists at this time for this car.'], Response::HTTP_CONFLICT);
+        }
+
+        $booking->save();
+
+        return BookingResource::make($booking);
+    }
+
+    public function update(Request $request, Booking $booking)
+    {
+        $requestedCar = Car::find($request->car_id);
+
+        $age = (int) $request->input('age');
+        $additionalCost = $this->agePolicyService->getAdditionCost($age);
+
+        $booking->date_from = $request->date_from;
+        $booking->date_to = $request->date_to;
+        $booking->car_id = $requestedCar->getKey();
+        $booking->cost = $requestedCar->cost + $additionalCost;
+
+        $overlappingBookingCount = $this->bookingRepository->countOverlappingExcludingId(
+            $booking->from_date, 
+            $booking->to_date,
+            $requestedCar,
+            $booking->getKey()
+        );
+
+        if ($overlappingBookingCount > 0) {
+            return response()->json(['error' => 'A booking already exists at this time for this car.'], Response::HTTP_CONFLICT);
+        }
+
+        $booking->save();
+
+        return BookingResource::make($booking)
+            ->response()
+            ->setStatusCode(Response::HTTP_CREATED);
+    }
+
+    public function destroy(Booking $booking)
+    {
+        $booking->delete();
+
+        return response()->json([], Response::HTTP_ACCEPTED);
+    }
+}
