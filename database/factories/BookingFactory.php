cf76b1214ed22fdd23e6ba1a31b73e28826bd1ee
Scott Drinkwater <s.p.drinkwater@gmail.com>
2024-03-08 12:57:58 +0000

    Populate factories

--- database/factories/BookingFactory.php
+++ database/factories/BookingFactory.php
@@ -16,8 +16,22 @@ class BookingFactory extends Factory
      */
     public function definition(): array
     {
+        $fromDate = $this->faker->dateTime();
+        $toDate = (clone $fromDate)->modify('+1 week');
+
         return [
-            //
+            // Should not be used for Factory::create, only Factory::make
+            'id' => $this->faker->unique()->numberBetween(0, PHP_INT_MAX),
+            'from_date' => $fromDate,
+            'to_date' => $toDate,
+            'cost' => $this->faker->numberBetween(10, 1000),
+            'car_id' => $this->faker->unique()->numberBetween(0, PHP_INT_MAX),
         ];
+    }
+
+
+    protected function withFaker()
+    {
+        return \Faker\Factory::create('en');
     }
 }
--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -17,7 +17,16 @@ class CarFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            // Should not be used for Factory::create, only Factory::make
+            'id' => $this->faker->unique()->numberBetween(0, PHP_INT_MAX),
+            'model' => $this->faker->company(),
+            'cost' => $this->faker->numberBetween(10, 1000),
+            'registration' => $this->faker->text(7),
         ];
+    }
+
+    protected function withFaker()
+    {
+        return \Faker\Factory::create('en');
     }
 }
